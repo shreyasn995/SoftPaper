9979101193aae7ba1d88d01d1cdd52b19d4f26bb
Shreyas Nagarajappa <u5834888@anu.edu.au>
2016-03-11 22:05:45 +1100

    Added funtionality to LoginActivity.

--- app/src/main/java/com/example/android/softpaper/LoginActivity.java
+++ app/src/main/java/com/example/android/softpaper/LoginActivity.java
@@ -1,13 +1,86 @@
 package com.example.android.softpaper;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.util.Calendar;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView welcomeText;
+    EditText password;
+    TextView loginError;
+
+    String presetPassphrase = null;
+
+    /* File for storing the login passphrase */
+    static final String filename_password = "loginPassword";
+    FileInputStream inputStream;
+    FileOutputStream outputStream;
+    File passwordFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        welcomeText = (TextView) findViewById(R.id.text_welcomeText);
+        password = (EditText) findViewById(R.id.edit_password);
+        loginError = (TextView) findViewById(R.id.text_loginError);
+
+        /* Get time from Android to display correct welcome text */
+        String textBuffer = welcomeText.getText().toString();
+        Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY); //Get current time in hours
+        if (hour < 12) textBuffer += " Morning!"; //It is before 12 noon
+        else if (hour < 18) textBuffer += " Afternoon!"; //It id before 6 p.m.
+        else textBuffer += " Evening!"; //It is After 6 p.m.
+        welcomeText.setText(textBuffer); //Set welcome text based on current time
+
+        /* Open file and read preset password */
+        passwordFile = new File(getFilesDir(), filename_password);
+        if (passwordFile.exists()) {
+            try {
+                inputStream = openFileInput(filename_password);
+                BufferedReader fetchPassword = new BufferedReader(new InputStreamReader(inputStream));
+                presetPassphrase = fetchPassword.readLine(); //Store the preset password in the PresetPassword string.
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    /* Compare retrieved password to entered password */
+    public void comparePassword(View view){
+        String enteredPassword = password.getText().toString();
+        /* If password never set before, write new password to file and launch the viewNotes Activity */
+        if (presetPassphrase == null){
+            try{
+                outputStream = openFileOutput(filename_password, Context.MODE_PRIVATE);
+                outputStream.write(enteredPassword.getBytes());
+                outputStream.close();
+                //Launch viewNotes Activity here
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            if (enteredPassword.equals(presetPassphrase)){
+                //Launch viewNotes Activity
+            }
+            /* else display error message */
+            else{
+                loginError.setText("Wrong passphrase entered. Try again.");
+            }
+        }
     }
 }
