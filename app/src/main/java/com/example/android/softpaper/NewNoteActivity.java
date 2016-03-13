99bf7031d733cc883509074dd7c50aa86698c12a
Shreyas Nagarajappa <u5834888@anu.edu.au>
2016-03-13 20:26:08 +1100

    Added service to receive text from other apps. Appealing UI modifications. Added funtionality to NewNoteActivity.

--- app/src/main/java/com/example/android/softpaper/NewNoteActivity.java
+++ app/src/main/java/com/example/android/softpaper/NewNoteActivity.java
@@ -1,13 +1,49 @@
 package com.example.android.softpaper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class NewNoteActivity extends AppCompatActivity {
+
+    EditText title;
+    EditText note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
+
+        title = (EditText) findViewById(R.id.edit_newNoteTitle);
+        note = (EditText) findViewById(R.id.edit_newNoteNote);
+
+        /**
+         * Get intent, action and MIME type.
+         * Code to get text from other apps sourced from:
+         * http://developer.android.com/training/sharing/receive.html
+          */
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                handleSendText(intent); // Handle text being sent
+            }
+            else {
+            // Handle other intents, such as being started from the home screen
+            }
+        }
+    }
+
+    void handleSendText(Intent intent) {
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText != null) {
+            // Update UI to reflect text being shared
+            note.setText(sharedText);
+        }
     }
 }
