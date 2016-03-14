15add3cf73ebd88f575518bd2a95d384c49e2c22
Shreyas Nagarajappa <u5834888@anu.edu.au>
2016-03-14 12:24:36 +1100

    Added Menu items.

--- app/src/main/java/com/example/android/softpaper/NewNoteActivity.java
+++ app/src/main/java/com/example/android/softpaper/NewNoteActivity.java
@@ -3,9 +3,9 @@ package com.example.android.softpaper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class NewNoteActivity extends AppCompatActivity {
 
@@ -24,7 +24,7 @@ public class NewNoteActivity extends AppCompatActivity {
          * Get intent, action and MIME type.
          * Code to get text from other apps sourced from:
          * http://developer.android.com/training/sharing/receive.html
-          */
+         */
         Intent intent = getIntent();
         String action = intent.getAction();
         String type = intent.getType();
@@ -34,9 +34,31 @@ public class NewNoteActivity extends AppCompatActivity {
                 handleSendText(intent); // Handle text being sent
             }
             else {
-            // Handle other intents, such as being started from the home screen
+                // Handle other intents, such as being started from the home screen
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_view_notes, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     void handleSendText(Intent intent) {
